2bc1d3336ef76678d0d152d2e17934cc318c945c
Wiz Lee <wizlee.dev@gmail.com>
2021-03-30 01:11:54 +0800

    Add example usage of CometChatUi
    - clone cometchat angular ui kit
    - install @ctrl/ngx-emoji-mart
    - generate an empty login component
    - temporary login in 'main.ts' and diretly use the full
    CometChatUi in 'app.component.ts'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CometChatUI } from "../cometchat-pro-angular-ui-kit/CometChatWorkspace/projects/angular-chat-ui-kit/src/components/CometChatUI/CometChat-Ui/cometchat-ui.module";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, CometChatUI],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -18,10 +18,18 @@ const appSetting = new CometChat.AppSettingsBuilder()
 CometChat.init(COMETCHAT_CONSTANTS.APP_ID, appSetting).then(
   () => {
     console.log("CometChat initialized successfully");
-    // You can now call login function.
-    platformBrowserDynamic()
-      .bootstrapModule(AppModule)
-      .catch((err) => console.error(err));
+
+    CometChat.login(COMETCHAT_CONSTANTS.UIDs.ironman, COMETCHAT_CONSTANTS.AUTH_KEY).then(
+      (user) => {
+        console.log("Login Successful:", { user });
+        platformBrowserDynamic()
+          .bootstrapModule(AppModule)
+          .catch((err) => console.error(err));
+      },
+      (error) => {
+        console.log("Login failed with exception:", { error });
+      }
+    );
   },
   (error) => {
     console.log("Initialization failed with error:", error);
