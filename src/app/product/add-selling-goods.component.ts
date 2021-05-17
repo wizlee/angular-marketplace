036cd6e5106394a1b7f16ba6debbe6e9772bcd7f
Wiz Lee <wizlee.dev@gmail.com>
2021-05-18 03:02:22 +0800

    Add selling goods page
    - Add the routing and auth guard to add selling goods page.
    Pending full UI and mechanism to add goods.
    - Add a better Home logo

---
+++ src/app/product/add-selling-goods.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './add-selling-goods.component.html',
+  styles: [
+  ]
+})
+export class AddSellingGoodsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -5,14 +5,26 @@ import { RouterModule } from "@angular/router";
 import { BasketComponent } from './basket.component';
 import { FaceMaskProductListComponent } from './face-mask-product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
+import { AddSellingGoodsComponent } from './add-selling-goods.component';
+import { AuthGuard } from '../account/auth.guard';
 
 @NgModule({
   declarations: [
     BasketComponent,
     FaceMaskProductListComponent,
     ProductDetailComponent,
+    AddSellingGoodsComponent,
   ],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: "addgoods",
+        component: AddSellingGoodsComponent,
+        canActivate: [AuthGuard],
+      },
+    ]),
+  ],
   exports: [BasketComponent],
 })
 export class ProductModule {}
