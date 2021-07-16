b81baa84d873291357c375e05a38ea38a9ad898a
Wiz Lee <wizlee.dev@gmail.com>
2021-07-17 01:49:43 +0800

    remove UserMessageComponent as it will cause error before CometChat Angular UI Kit is setup

--- angular-marketplace-start/src/app/chat/chat.module.ts
+++ angular-marketplace-start/src/app/chat/chat.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InboxComponent } from './inbox.component';
-import { UserMessageComponent } from './user-message.component';
 
 @NgModule({
-  declarations: [InboxComponent, UserMessageComponent],
+  declarations: [InboxComponent],
   imports: [
     CommonModule,
   ],
-  exports: [InboxComponent, UserMessageComponent],
+  exports: [InboxComponent],
 })
 export class ChatModule {}
